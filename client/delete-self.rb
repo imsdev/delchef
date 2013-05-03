f0906d21aa3211cf06b88d0b111a27504b6e3baa
Max Dietz <mdietz@wesleyan.edu>
2013-05-03 14:28:19 -0400

    typos

--- client/delete-self.rb
+++ client/delete-self.rb
@@ -2,6 +2,6 @@
 
 require 'socket'
 # SERVER_NAME is the name of the server
-# and PORT is the port number specified in del-chef.c
+# and PORT is the port number specified in server/delchef.c
 s = TCPSocket.new(SERVER_NAME,PORT)
 s.close
